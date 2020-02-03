311d73282e9cbfe1832ed1cb6c1f7c9a2f0f950e
MrDJEnz <djenzmann@gmail.com>
2020-02-02 23:43:15 -0500

    adding in some basic SQL info

--- csv_to_db.py
+++ csv_to_db.py
@@ -1,15 +1,77 @@
 import sqlite3
 import pandas as pd
+import os.path
+from os import path
 
-# load data
-df = pd.read_csv('top50.csv')
+def convert():
 
-# strip whitespace from headers
-df.columns = df.columns.str.strip()
+    if path.exists("top50.db"):
+        print("Err file already exists")
+    else:
+        # load data
+        df = pd.read_csv('top50.csv')
 
-con = sqlite3.connect("top50.db")
+        # strip whitespace from headers
+        df.columns = df.columns.str.strip()
 
-# drop data into database
-df.to_sql("MyTable", con)
+        con = sqlite3.connect("top50.db")
 
-con.close()
+        # drop data into database
+        df.to_sql("MyTable", con)
+
+        con.close()
+
+        print("Database successfully created.")
+
+def connection_to_db(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = sqlite3.connect(db_file)
+
+    return conn
+
+def printdb():
+    pathDB = "top50.db"
+    connection = connection_to_db(pathDB)
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM 'MyTable' LIMIT 0,30")
+    row = cursor.fetchone()
+    while row is not None:
+        id = row[0]
+        rank = row[1]
+        trackname = row[2]
+        print("id: " + str(id) + "    rank: " + str(rank) + "    trackname: " + str(trackname))
+        row = cursor.fetchone()
+
+    connection.commit()
+
+def basic_english_to_sql():
+    loop = True
+    while loop:
+        usrinput = input("\nPlease enter information for pizza: ")
+        if usrinput == "help":
+            print("\nOptions for user:")
+            print("1) Type ' ____ 'for ____.")
+            print("2) Type 'newdb' for new db creation.")
+            print("3) Type 'help' for this menu.")
+            print("4) Type 'quit' to exit.\n")
+        elif usrinput == "printdb":
+            printdb()
+        elif usrinput == "newdb":
+            convert()
+        elif usrinput == "____":
+            #TODO
+            print("Do stuff")
+        elif usrinput == "quit":
+            print("Quitting the program :)")
+            loop = False
+        else:
+            print("invalid message please type help for help")
+
+
+basic_english_to_sql()

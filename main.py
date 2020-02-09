be2f6d397bd25ce229a7e8096364c46c0f814ce5
LLcoolDrake <LLcoolDrake@users.noreply.github.com>
2020-02-09 16:54:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,6 @@ def main():
 
     #
     
-    ###################### JD 
-    #connection = sqlite3.connect("Pizza.db")
-
-    #pointer = connection.cursor()
-    #TODO: need to handle Tables being created only once
-
-    #connection.execute('''CREATE TABLE PizzaPrimary ([Id] INTEGER PRIMARY KEY,[State] text, [PostalCode] integer,[Categories] text, [PriceRangeMax] integer)''')
-          
-    #connection.execute('''CREATE TABLE PizzaSecondary([Id]INTEGER PRIMARY KEY,[City] text,[Name] integer,[Address] text, [Latitude] integer,[Longitude] integer)''')
-
-    #read_PizzaPrim = pd.read_csv (r'PizzaPrim.csv')
-    #read_PizzaPrim.to_sql('PizzaPrimary', connection, if_exists='append', index = False) 
-    # Insert the values from the csv file into the table 'CLIENTS' 
-
-    #read_PizzaNotPrim = pd.read_csv (r'PizzaNotPrim.csv')
-    #read_PizzaNotPrim.to_sql('PizzaSecondary', connection, if_exists='replace', index = False) # Replace the values from the csv file into the table 'COUNTRY'
-    
-    ########################### JD
-    
-    
-    
-    
     
     
     
@@ -82,30 +60,69 @@ def main():
 def help():
     print("help screen ... example command")
     print("FORMAT TO PARSE DATA AS FOLLOWS: ...")
+    print(" ")
 
-    print("SONG LENGTH") ## NEED TO UPDATE TO 'pizza' syntax 
-    print("SONG ARTIST")
-    print("SONG GENRE")
-    print("ARTIST ...")
+    print("_____________________")
+    print("Pizza Primary Table")
+    print("--------------------")
+    print("State")
+    print("PostalCode")
+    print("Categories")
+    print("PriceRangeMax")
+    print("______________________")
+    print("Pizza Secondary Table")
+    print("---------------------")
+    print("City") ## NEED TO UPDATE TO 'pizza' syntax 
+    print("Name")
+    print("Address")
+    print("Latitude")
+    print("Longitude")
+    print("")
 
 
 def convert():
 
-    if path.exists("top50.db"):
+    #Checks to see if Pizza.db exists
+    # if the the db already exitsted and we ran the create table
+    # function, we would return an error
+
+    if path.exists("Pizza.db"):
         print("Err file already exists")
     else:
-        # load data
-        df = pd.read_csv('top50.csv')
 
-        # strip whitespace from headers
-        df.columns = df.columns.str.strip()
 
-        con = sqlite3.connect("top50.db")
+      # Connecting to the database, this also seems to 
+      #crate a Pizza.db file, not quite sure what the black box
+      # is doing. .db might be special or it might just be an empty file
+      connection = sqlite3.connect("Pizza.db")
 
-        # drop data into database
-        df.to_sql("MyTable", con)
 
-        con.close()
+      # creates a pointer, not quite sure what it really is
+      pointer = connection.cursor()
+
+
+      # This is where we create a table that will exist inside of 
+      # Pizza.db
+      connection.execute('''CREATE TABLE PizzaPrimary ([Id] INTEGER PRIMARY KEY,[State] text, [PostalCode] integer,[Categories] text, [PriceRangeMax] integer)''')
+          
+      connection.execute('''CREATE TABLE PizzaSecondary([Id]INTEGER PRIMARY KEY,[City] text,[Name] integer,[Address] text, [Latitude] integer,[Longitude] integer)''')
+
+      read_PizzaPrim = pd.read_csv (r'PizzaPrim.csv')
+      read_PizzaPrim.to_sql('PizzaPrimary', connection, if_exists='append', index = False) 
+    # Insert the values from the csv file into the table 'CLIENTS' 
+
+      read_PizzaNotPrim = pd.read_csv (r'PizzaNotPrim.csv')
+      read_PizzaNotPrim.to_sql('PizzaSecondary', connection, if_exists='replace', index = False) # Replace the values from the csv file into the table 'COUNTRY'
+
+      #df = pointer.execute("SELECT * FROM PizzaPrim")
+      #print(df)
+
+      print("DataBase loaded to LocalHost")
+
+      pointer.close()
+      connection.close()
+
+      return 
 
         print("Database successfully created.")
 
@@ -121,21 +138,62 @@ def connection_to_db(db_file):
     return conn
 
 def printdb():
-    pathDB = "top50.db"
+    # if we call printdb before calling newdb, we create an error
+    # we also create pizza.db without anything inside of it
+ 
+    # Creates Path variable
+    
+    if path.exists("Pizza.db"):
+      # i was getting indentation errors here
+      # so i added a dumb variable
+      # python probably doesnt like empty if statments
+      # itd be nice if we could figure out if a .notExits function
+      # exists, not quite sure how to find the function defintion
+      # documentation for SQl3lite in python
+      i =3
+       # nice the table exits
+    else:
+      # if db is not set up yet calls connection method
+
+      convert()
+       
+
+
+    # Creates Path variable
+    pathDB = "Pizza.db"
+
+    #Creates connection to path variable
     connection = connection_to_db(pathDB)
 
+    #Creates a cursor/pointer. I think this is like  
+    # the >>> lines that would be in the SQLlite3 termial
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM 'MyTable' LIMIT 0,30")
-    row = cursor.fetchone()
-    while row is not None:
-        id = row[0]
-        rank = row[1]
-        trackname = row[2]
-        print("id: " + str(id) + "    rank: " + str(rank) + "    trackname: " + str(trackname))
-        row = cursor.fetchone()
+    #This is where you execute the query
+    #Node that this query is surrounded by ""
+    # Ive noticed that some SQL commands like the create table
+    # above, uses ''' ''', so if your having trouble trying to 
+    #experiment doing something, maybe check that first
+    cursor.execute("SELECT * FROM PizzaPrimary LIMIT 10;")
 
+
+    # the cursor doesnt print anything, you need to 
+    # fecth the results from the blackbox where ever it s
+    results=cursor.fetchall()
+
+    # then you can print the querey variable that you created
+    print(results)
+
+
+    # not sure what this is doing
     connection.commit()
+
+    #Closing connections, online blurb said
+    # sometimes if you don't close the connections it crates 
+    # problems
+    cursor.close()
+    connection.close()
+
 
 
 # test user command syntax
